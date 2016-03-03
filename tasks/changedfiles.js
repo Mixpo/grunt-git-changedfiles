4a61aa4b172d54af838bd44b77d04f08c694dfc4
Max Winderbaum <maxwinderbaum@gmail.com>
2016-03-03 09:43:29 -0800

    Diff with develop~1 so we can check merges

--- tasks/changedfiles.js
+++ tasks/changedfiles.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
         var done = this.async();
         grunt.util.spawn({
             cmd: 'git',
-            args: ['diff', 'develop', '--name-only', '--diff-filter=ACM'] // staged and working tree files
+            args: ['diff', 'develop~1', '--name-only', '--diff-filter=ACM'] // staged and working tree files
         }, function(error, result){
 
             var changedFiles = String(result).split(grunt.util.linefeed);
